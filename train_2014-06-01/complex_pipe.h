09aad0aa6f3481433b4712c37addc6f5deed0a20
georgeee <george.agapov@gmail.com>
2014-06-12 13:33:25 +0400

    train_2014-06-01 first stable version

--- train_2014-06-01/complex_pipe.h
+++ train_2014-06-01/complex_pipe.h
@@ -27,53 +27,48 @@ int _execute(struct exec_args_t exec_args){
     return 0;
 }
 
+int _execute_with_inout(struct exec_args_t exec_args, int in_fd, int out_fd){
+    int ret = dup2(in_fd, STDIN_FILENO);
+    if(ret < 0) return -1;
+    ret = dup2(out_fd, STDOUT_FILENO);
+    if(ret < 0) return -1;
+    return _execute(exec_args);
+}
+
+int _pipe_closeonexec(int pipefds[2]){
+    int ret = pipe(pipefds);
+    if(ret < 0) return -1;
+    ret = fcntl(pipefds[0], F_SETFD, FD_CLOEXEC);
+    if(ret < 0) return -1;
+    ret = fcntl(pipefds[1], F_SETFD, FD_CLOEXEC);
+    if(ret < 0) return -1;
+    return 0;
+}
 
 int init_complex_pipe(struct complex_pipe_t * complex_pipe, struct exec_args_t exec_args){
     int pipefds_left[2];
     int pipefds_right[2];
-    int ret = pipe(pipefds_left);
+    int ret = _pipe_closeonexec(pipefds_left);
     if(ret < 0) return -1;
-    ret = pipe(pipefds_right);
+    ret = _pipe_closeonexec(pipefds_right);
     if(ret < 0) return -1;
-    ret = fcntl(pipefds_left[0], F_SETFD, FD_CLOEXEC);
-    if(ret < 0) return -1;
-    ret = fcntl(pipefds_left[1], F_SETFD, FD_CLOEXEC);
-    if(ret < 0) return -1;
-    ret = fcntl(pipefds_right[0], F_SETFD, FD_CLOEXEC);
-    if(ret < 0) return -1;
-    ret = fcntl(pipefds_right[1], F_SETFD, FD_CLOEXEC);
-    if(ret < 0) return -1;
-    ret = dup2(pipefds_left[0], STDIN_FILENO);
-    if(ret < 0) return -1;
-    ret = dup2(pipefds_right[1], STDOUT_FILENO);
-    if(ret < 0) return -1;
+    ret = _execute_with_inout(exec_args, pipefds_left[0], pipefds_right[1]);
     complex_pipe->left_fd = pipefds_left[1];
     complex_pipe->right_fd = pipefds_right[0];
-    ret = _execute(exec_args);
     if(ret < 0) return -1;
     ret = close(pipefds_left[0]);
     if(ret < 0) return -1;
     ret = close(pipefds_right[1]);
     if(ret < 0) return -1;
-    //fprintf(stderr, "init_complex_pipe(complex_pipe : {l: %d, r: %d}, exec_args.file = %s)\n", complex_pipe->left_fd, complex_pipe->right_fd, exec_args.file);
     return 0;
 }
 
 int bind_right(struct complex_pipe_t * complex_pipe, struct exec_args_t exec_args){
     int pipefds_right[2];
-    int ret = pipe(pipefds_right);
+    int ret = _pipe_closeonexec(pipefds_right);
     if(ret < 0) return -1;
-    ret = fcntl(pipefds_right[0], F_SETFD, FD_CLOEXEC);
-    if(ret < 0) return -1;
-    ret = fcntl(pipefds_right[1], F_SETFD, FD_CLOEXEC);
-    if(ret < 0) return -1;
-    ret = dup2(complex_pipe->right_fd, STDIN_FILENO);
-    if(ret < 0) return -1;
-    ret = dup2(pipefds_right[1], STDOUT_FILENO);
-    if(ret < 0) return -1;
-    //fprintf(stderr, "bind_right(complex_pipe : {l: %d, r: %d} => {l: %d, r: %d}, exec_args.file = %s)\n", complex_pipe->left_fd, complex_pipe->right_fd, complex_pipe->left_fd, pipefds_right[0], exec_args.file);
+    ret = _execute_with_inout(exec_args, complex_pipe->right_fd, pipefds_right[1]);
     complex_pipe->right_fd = pipefds_right[0];
-    ret = _execute(exec_args);
     if(ret < 0) return -1;
     ret = close(pipefds_right[1]);
     if(ret < 0) return -1;
@@ -82,26 +77,17 @@ int bind_right(struct complex_pipe_t * complex_pipe, struct exec_args_t exec_arg
 
 int bind_left(struct complex_pipe_t * complex_pipe, struct exec_args_t exec_args){
     int pipefds_left[2];
-    int ret = pipe(pipefds_left);
+    int ret = _pipe_closeonexec(pipefds_left);
     if(ret < 0) return -1;
-    ret = fcntl(pipefds_left[0], F_SETFD, FD_CLOEXEC);
-    if(ret < 0) return -1;
-    ret = fcntl(pipefds_left[1], F_SETFD, FD_CLOEXEC);
-    if(ret < 0) return -1;
-    ret = dup2(pipefds_left[0], STDIN_FILENO);
-    if(ret < 0) return -1;
-    ret = dup2(complex_pipe->left_fd, STDOUT_FILENO);
-    if(ret < 0) return -1;
-    //fprintf(stderr, "bind_left(complex_pipe : {l: %d, r: %d} => {l: %d, r: %d}, exec_args.file = %s)\n", complex_pipe->left_fd, complex_pipe->right_fd, pipefds_left[1], complex_pipe->right_fd, exec_args.file);
+    ret = _execute_with_inout(exec_args, pipefds_left[0], complex_pipe->left_fd);
     complex_pipe->left_fd = pipefds_left[1];
-    ret = _execute(exec_args);
     if(ret < 0) return -1;
     ret = close(pipefds_left[0]);
     if(ret < 0) return -1;
     return 0;
 }
 
-int launch_inout_dumper(){
+int launch_inout_dumper(int in_fd, int out_fd){
     struct exec_args_t args;
     args.file = "cat";
     char tmp[2][10];
@@ -110,33 +96,15 @@ int launch_inout_dumper(){
     args.argv[0] = tmp[0];
     args.argv[1] = tmp[1];
     args.argv[2] = NULL;
-    return _execute(args);
+    return _execute_with_inout(args, in_fd, out_fd);
 }
 
 int finalize_input(struct complex_pipe_t * complex_pipe, int fd){
-    /*
-     * We need to 1) bind stdin to fd
-     * 2) bind stdout to complex_pipe's write end
-     * 3) launch 'cat -'
-     */
-    int ret = dup2(fd, STDIN_FILENO);
-    if(ret < 0) return -1;
-    ret = dup2(complex_pipe->left_fd, STDOUT_FILENO);
-    if(ret < 0) return -1;
-    return launch_inout_dumper();
+    return launch_inout_dumper(fd, complex_pipe->left_fd);
 }
 
 int finalize_output(struct complex_pipe_t * complex_pipe, int fd){
-    /* We need to
-     * 1) bind stdin to complex_pipe's read end
-     * 2) bind stdout to fd
-     * 3) launch 'cat -'
-     */
-    int ret = dup2(complex_pipe->right_fd, STDIN_FILENO);
-    if(ret < 0) return -1;
-    ret = dup2(fd, STDOUT_FILENO);
-    if(ret < 0) return -1;
-    return launch_inout_dumper();
+    return launch_inout_dumper(complex_pipe->right_fd, fd);
 }
 
 struct exec_args_t create_exec_args(const char * cmd){
